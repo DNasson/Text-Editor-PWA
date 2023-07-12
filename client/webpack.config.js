1101ac5d2f5e5fdbaceb2cf5fc886e9d4afa0aa8
FIRST_NAME LAST_NAME <daniellememmott@hotmail.com>
2023-07-11 21:28:20 -0600

    "add workbox plugins, add CSS loaders and babel"

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,29 +1,68 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "/index.html",
+        title: "Jate",
+      }),
+
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "sw.js",
+      }),
+
+      new WebpackPwaManifest({
+        name: "Jate",
+        short_name: "Jate",
+        description: "Jate is a simple note taking app",
+        background_color: "#ffffff",
+        theme_color: "#2196f3",
+        crossorigin: "use-credentials",
+        icons: [
+          {
+            src: path.resolve("src/assets/icon.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              plugins: ["@babel/plugin-transform-runtime"],
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
       ],
     },
   };
