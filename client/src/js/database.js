e318c80fa88c2c69604ad0483d8634c343653e87
FIRST_NAME LAST_NAME <daniellememmott@hotmail.com>
2023-07-11 22:00:46 -0600

    "fix package.json scripts"

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,16 +14,16 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (id) => {
-  console.log('ADD to the database', id);
+  console.log('PUT to the database', id);
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('contact', 1);
+  const contactDb = await openDB('content', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('contact', 'readwrite');
+  const tx = contactDb.transaction('content', 'readwrite');
 
   // Open up the desired object store.
-  const store = tx.objectStore('contact');
+  const store = tx.objectStore('content');
 
   // Use the .put() method to get all data in the database.
   const request = store.put(id);
