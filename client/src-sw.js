a45b9ada31d5be485a6aa7150775aef239a49275
DMemmott <daniellememmott@hotmail.com>
2023-08-16 21:54:44 -0600

    "remove todos, fix asset caching"

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
-const { CacheFirst } = require("workbox-strategies");
+const { StaleWhileRevalidate, CacheFirst } = require("workbox-strategies");
 const { registerRoute } = require("workbox-routing");
 const { CacheableResponsePlugin } = require("workbox-cacheable-response");
 const { ExpirationPlugin } = require("workbox-expiration");
@@ -26,10 +26,12 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
-// TODO: Implement asset caching
+// Implement asset caching
 registerRoute(
+  // define which assets to cache
   ({ request }) => ["style", "script", "worker"].includes(request.destination),
-  new CacheFirst({
+  new StaleWhileRevalidate({
+
     cacheName: "asset-cache",
     plugins: [
       new CacheableResponsePlugin({
