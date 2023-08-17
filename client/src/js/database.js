030fd3dc18afaac39e2d9dbbb8e4785d9c74858c
DMemmott <daniellememmott@hotmail.com>
2023-08-16 21:46:57 -0600

    "fix content to contact"

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,23 +17,23 @@ export const putDb = async (id) => {
   console.log('PUT to the database', id);
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('content', 1);
+  const contactDb = await openDB('contact', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('content', 'readwrite');
+  const tx = contactDb.transaction('contact', 'readwrite');
 
   // Open up the desired object store.
-  const store = tx.objectStore('content');
+  const store = tx.objectStore('contact');
 
   // Use the .put() method to get all data in the database.
-  const request = store.put(id);
+  const request = store.put({ id:1, value: content });
 
   // Get confirmation of the request.
   const result = await request;
-  console.log('result.value', result);
-  return result?.value;
+  console.log('Data saved!', result);
+  
 };
-// TODO: Add logic for a method that gets all the content from the database
+// Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
 
@@ -54,4 +54,5 @@ export const getDb = async () => {
   console.log('result.value', result);
   return result;
 };
+
 initdb();
