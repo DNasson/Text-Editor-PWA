bd917c472e0f146469f9da591dca9c5a464d68eb
DMemmott <daniellememmott@hotmail.com>
2023-08-16 21:50:55 -0600

    "remove todos, add comments"

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,25 +1,31 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
+// Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
+    // Store the event for later use
     window.defferedPrompt = event;
+
+    // Remove the `hidden` attribute from the install button
     butInstall.removeAttribute('hidden', false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
+// Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.defferedPrompt;
     if (!promptEvent) return;
 
+// show promp
     promptEvent.prompt();
 
+// reset prompt vairable
     window.defferedPrompt = null;
 
     butInstall.setAttribute('hidden', true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
+// Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    console.log('Jate was installed.', event);
+
+    window.defferedPrompt = null;
 });
